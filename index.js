065d6748e6930427cc73d4309ceac21b9efd23b8
uradora <meri.rajamaki@gmail.com>
2021-05-23 01:51:56 +0300

    build copied

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
