d4f06efb31f2edc1c7e1629d453981e999653d4a
uradora <meri.rajamaki@gmail.com>
2021-05-23 03:53:49 +0300

    mongoose modelin lisääminen ja dotenv konfigurointi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
+const Person = require('./models/person')
 
 let persons = [
   {
@@ -37,29 +39,35 @@ let persons = [
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).end()
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
-  const newId = Math.floor(Math.random() * 1000000)
-
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -68,25 +76,61 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(409).json({
-      error: 'name must be unique'
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson)
     })
-  }
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
   const person = {
     name: body.name,
     number: body.number,
-    id: newId
   }
-  persons = persons.concat(person)
-  res.json(person)
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/info', (req, res) => {
   const date = new Date()
-  res.json(`Phonebook has info for ${persons.length} people \n ${date}`)
+  Person.find({}).then((persons) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <br><p>${date}</p>`)
+  })
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" })
+}
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
