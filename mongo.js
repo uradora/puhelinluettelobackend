0f59d7463f517e68063e0713cb4fb4b45a300323
uradora <meri.rajamaki@gmail.com>
2021-05-23 03:22:37 +0300

    lisäsin mongodb -tietokannan

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://meri:${password}@cluster0.623ww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv[3]) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then((result) => {
+    console.log(`${person.name} ${person.number}`)
+    mongoose.connection.close()
+  })
+}
+
+if (!process.argv[3]) {
+  console.log("people:")
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
